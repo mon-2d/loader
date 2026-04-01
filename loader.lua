e290ed62e25c2425908a16f5c4704e818f427796
mon <ko0416595@gmail.com>
2026-04-01 10:37:19 +0700

    Update loader URL for Lua script

--- loader.lua
+++ loader.lua
@@ -10,4 +10,4 @@ end;
 if (game.PlaceId == 2788229376) then
 	loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/18167a704b494688866bd5f3aa9da862.lua"))()
 end;
-loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/ed5aad42d32df68ce1fe0f1919f7e097.lua"))();
+loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/9d86ca5713fcc3d793c377fbbd93fdf6.lua"))()
