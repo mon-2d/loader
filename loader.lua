aee494d65f46574b6f660c739ab3f60a3276c675
mon <ko0416595@gmail.com>
2026-04-11 10:39:38 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,13 +1,20 @@
 if (getgenv().UC_LOADED) then
-	return;
+    return;
 end;
 getgenv().UC_LOADED = true;
+
 if (identifyexecutor() == "Wave") then
-	getgenv().gethui = function()
-		return game:GetService("CoreGui");
-	end;	
+    getgenv().gethui = function()
+        return game:GetService("CoreGui");
+    end;    
 end;
+
 if (game.PlaceId == 2788229376) then
-	loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/ff0850aeaef1c7f649d5bd3acf995976.lua"))()
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/ff0850aeaef1c7f649d5bd3acf995976.lua" ))()
 end;
-loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/5e0225ad1ab8668fb102acc2a4e64bc9.lua"))()
+
+if (game.PlaceId == 155615604) then
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/4e79dc4ab9adddae25279dc271aa9e97.lua" ))()
+end;
+
+loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/5e0225ad1ab8668fb102acc2a4e64bc9.lua" ))()
